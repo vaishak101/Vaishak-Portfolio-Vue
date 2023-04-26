27eb83cfffd15f3dfb1c66f29a43a7da684f2062
Vaishak Nair <nairvaishak101@gmail.com>
2023-04-26 17:51:39 +0530

    Work Section Added

--- src/components/slide/work/WorkSlide.vue
+++ src/components/slide/work/WorkSlide.vue
@@ -12,60 +12,121 @@
         ><span class="ph__span">T</span><span class="ph__span">S</span>
       </h1>
       <div class="work__container">
-        <div class="work__card--row">
-          <div class="work__card">
-            <span class="work__card--span-1">Elegant Design</span>
-            <span class="work__card--span-2">#Project 1</span>
-            <div class="work__card--container">
-              <img src=assets/img/whitestone-1.webp class=work__card--img
-              alt="White Stone Website Image">
-            </div>
-            <p class="work__card--desc">
-              Elegant personality for strong companies
-            </p>
-            <a class="work__card__btn" data-id="0">View</a>
+        <div class="work__card">
+          <span class="work__card--span-1">Elegant Design</span>
+          <span class="work__card--span-2">#Project 1</span>
+          <div class="work__card--container">
+            <img
+              src="./../../../assets/img/work/whitestone-1.webp"
+              class="work__card--img"
+              alt="White Stone Website
+            Image"
+            />
           </div>
-          <div class="work__card">
-            <span class="work__card--span-1"> Minimalist Design</span>
-            <span class="work__card--span-2">#Project 2</span>
-            <div class="work__card--container">
-              <img src=assets/img/citaz-0.jpg class=work__card--img alt="Citaz
-              bank website image">
-            </div>
-            <p class="work__card--desc">
-              Minimalist and light personality website
-            </p>
-            <a class="work__card__btn" data-id="1">View</a>
+          <p class="work__card--desc">
+            Static Webpage Styled In Elegant Design.
+          </p>
+          <div class="word__card--cta-wrap">
+            <a
+              href="https://github.com/vaishak101/white-stone"
+              target="_blank"
+              class="work__card__btn"
+              >Git Hub</a
+            >
+            <a
+              href="https://vaishak101.github.io/white-stone/"
+              target="_blank"
+              class="work__card__btn"
+              >View Page</a
+            >
           </div>
         </div>
-        <div class="work__card--row">
-          <div class="work__card">
-            <span class="work__card--span-1">Bold Design</span>
-            <span class="work__card--span-2">#Project 3</span>
-            <div class="work__card--container">
-              <img src=assets/img/revhead-0.webp class=work__card--img
-              alt="Revhead Motors webssite Img">
-            </div>
-            <p class="work__card--desc">
-              Bold personality with animations and effects
-            </p>
-            <a class="work__card__btn" data-id="2">View</a>
+        <div class="work__card">
+          <span class="work__card--span-1">Next.js App</span>
+          <span class="work__card--span-2">#Project 2</span>
+          <div class="work__card--container">
+            <img
+              src="./../../../assets/img/work/citaz-0.jpg"
+              class="work__card--img"
+              alt="Citaz
+            bank website image"
+            />
           </div>
-          <div class="work__card">
-            <span class="work__card--span-1">Todo List</span>
-            <span class="work__card--span-2">#Project 4</span>
-            <div class="work__card--container">
-              <div class="work__card--todo">
-                <img src=assets/img/todo-0.webp class="work__card--todo--img
-                work__card--todo--img-1" alt="V-todo website img "> <img
-                src=assets/img/todo-1.webp class="work__card--todo--img
-                work__card--todo--img-2" alt="V-todo website img ">
-              </div>
+          <p class="work__card--desc">
+            A Minimalistic Design Website Built With Next.js
+          </p>
+          <div class="word__card--cta-wrap">
+            <a class="work__card__btn">Git Hub</a>
+            <a class="work__card__btn">View Page</a>
+          </div>
+        </div>
+        <div class="work__card">
+          <span class="work__card--span-1">Bold Design</span>
+          <span class="work__card--span-2">#Project 3</span>
+          <div class="work__card--container">
+            <img
+              src="./../../../assets/img/work/revhead-0.webp"
+              class="work__card--img"
+              alt="Revhead Motors webssite Img"
+            />
+          </div>
+          <p class="work__card--desc">Static Webpage Styled In Bold Design</p>
+          <div class="word__card--cta-wrap">
+            <a
+              href="https://github.com/vaishak101/revhead"
+              target="_blank"
+              class="work__card__btn"
+              >Git Hub</a
+            >
+            <a
+              href="https://vaishak101.github.io/revhead/"
+              target="_blank"
+              class="work__card__btn"
+              >View Page</a
+            >
+          </div>
+        </div>
+        <div class="work__card">
+          <span class="work__card--span-1">Todo List</span>
+          <span class="work__card--span-2">#Project 4</span>
+          <div class="work__card--container">
+            <div class="work__card--todo">
+              <img
+                src="./../../../assets/img/work/todo-0.webp"
+                class="work__card--todo--img work__card--todo--img-1"
+                alt="V-todo website img "
+              />
+              <img
+                src="./../../../assets/img/work/todo-1.webp"
+                class="work__card--todo--img work__card--todo--img-2"
+                alt="V-todo website img "
+              />
             </div>
-            <p class="work__card--desc">
-              A todo list that performs CRUD & uses local storage
-            </p>
-            <a class="work__card__btn" data-id="3">View</a>
+          </div>
+          <p class="work__card--desc">
+            Todo App Build using React which performs CRUD operation.
+          </p>
+          <div class="word__card--cta-wrap">
+            <a class="work__card__btn">Git Hub</a>
+            <a class="work__card__btn">View Page</a>
+          </div>
+        </div>
+        <div class="work__card">
+          <span class="work__card--span-1">MERN Stack App</span>
+          <span class="work__card--span-2">#Project 4</span>
+          <div class="work__card--container">
+            <div class="work__card--todo">
+              <img
+                src=""
+                class="work__card--todo--img work__card--todo--img-1"
+                alt="LunchBOX"
+              />
+            </div>
+          </div>
+          <p class="work__card--desc">Lunchbox</p>
+          <div class="word__card--cta-wrap">
+            <a class="work__card__btn">Git Hub</a>
+            <a class="work__card__btn">View Page</a>
           </div>
         </div>
       </div>
